ef96ef90c7ae566335f278ef98a755a7ed14b57f
Ahmet Kara <ahmetk1294@gmail.com>
2022-06-14 18:05:39 +0300

    Update diagonal_difference.py

--- diagonal_difference.py
+++ diagonal_difference.py
@@ -1,3 +1,32 @@
+
+Skip to content
+Pull requests
+Issues
+Marketplace
+Explore
+@ahmetQara
+ahmetQara /
+diagonal_difference
+Public
+
+Code
+Issues
+Pull requests
+Actions
+Projects
+Wiki
+Security
+Insights
+
+    Settings
+
+diagonal_difference/diagonal_difference.py /
+@ahmetQara
+ahmetQara Update diagonal_difference.py
+Latest commit 81f0285 4 minutes ago
+History
+1 contributor
+70 lines (44 sloc) 1.31 KB
 #!/bin/python3
 
 import math
@@ -23,14 +52,14 @@ def diagonalDifference(arr):
     right_to_left = 0
     result_sum = 0;
     
-    for i in range (0,3):
+    for i in range (n):
         
         print("arr[i][i] = ", arr[i][i])
         left_to_right+=arr[i][i]
     
-    for j in range (0,3):
+    for j in range (n):
         
-        for k in range(2-j, -1, -1):
+        for k in range(n-1-j, -1, -1):
             
             right_to_left+=arr[j][k]
         
@@ -39,17 +68,17 @@ def diagonalDifference(arr):
             
             break
         
-    result_sum = right_to_left - left_to_right 
+    result_sum = abs(left_to_right-right_to_left) 
     
     return result_sum
 
 # short solution
     '''
 def diagonalDifference(arr):
-
     diagonal1 = sum(arr[x][x] for x in range(n))
     diagonal2= sum(arr[x][n-1-x] for x in range(n))
     return abs(diagonal1-diagonal2)
+    
     '''
             
 if __name__ == '__main__':
@@ -67,3 +96,19 @@ if __name__ == '__main__':
     fptr.write(str(result) + '\n')
 
     fptr.close()
+
+    © 2022 GitHub, Inc.
+
+    Terms
+    Privacy
+    Security
+    Status
+    Docs
+    Contact GitHub
+    Pricing
+    API
+    Training
+    Blog
+    About
+
+Loading complete
