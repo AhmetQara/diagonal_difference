7a78d9e7057567696f07cd76b120ae9b401cb8a8
Ahmet Kara <ahmetk1294@gmail.com>
2022-06-14 17:53:07 +0300

    Update diagonal_difference.py

--- diagonal_difference.py
+++ diagonal_difference.py
@@ -13,44 +13,13 @@ import sys
 # The function accepts 2D_INTEGER_ARRAY arr as parameter.
 #
 
-
-# long solution
-
-def diagonalDifference(arr):
-    
-
-    left_to_right = 0
-    right_to_left = 0
-    result_sum = 0;
-    
-    for i in range (0,3):
-        
-        print("arr[i][i] = ", arr[i][i])
-        left_to_right+=arr[i][i]
-    
-    for j in range (0,3):
-        
-        for k in range(2-j, -1, -1):
-            
-            right_to_left+=arr[j][k]
-        
-            #print("right_to_left = ",  right_to_left)
-            print("arr[j][k] = ",  arr[j][k])
-            
-            break
-        
-    result_sum = right_to_left - left_to_right 
-    
-    return result_sum
-
 # short solution
-    '''
+    
 def diagonalDifference(arr):
 
     diagonal1 = sum(arr[x][x] for x in range(n))
     diagonal2= sum(arr[x][n-1-x] for x in range(n))
     return abs(diagonal1-diagonal2)
-    '''
             
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
